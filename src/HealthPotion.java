4d25c83ac89326949598b60d07f31c155b4ac002
Christopher M. Wood <cwood@qualtrax.com>
2017-10-28 18:56:00 -0400

    Added player state and improvements in general

---
+++ src/HealthPotion.java
@@ -0,0 +1,7 @@
+public class HealthPotion 
+{
+	public void Use()
+	{
+		
+	}
+}
--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,72 @@
+import java.util.ArrayList;
+
 public class Player 
 {
 	public final String Name;
-	private int Health;
+	private final ArrayList<HealthPotion> healthPotions;
+	private final ArrayList<PoisonPotion> poisonPotions;
+	private final ArrayList<StrengthPotion> strengthPotions;
+	private int health;
+	private PlayerState state;
 	
 	public Player(String name)
 	{
 		Name = name;
-		Health = 100;
+		state = PlayerState.NONE;
+		health = 100;
+		healthPotions = new ArrayList<HealthPotion>();
+		healthPotions.add(new HealthPotion());
+		healthPotions.add(new HealthPotion());
+		healthPotions.add(new HealthPotion());
+		
+		poisonPotions = new ArrayList<PoisonPotion>();
+		poisonPotions.add(new PoisonPotion());
+		poisonPotions.add(new PoisonPotion());
+		poisonPotions.add(new PoisonPotion());
+		
+		strengthPotions = new ArrayList<StrengthPotion>();
+		strengthPotions.add(new StrengthPotion());
+		strengthPotions.add(new StrengthPotion());
+		strengthPotions.add(new StrengthPotion());
 	}
 	
 	public int Health()
 	{
-		return Health;
+		return health;
+	}
+	
+	public PlayerState State()
+	{
+		return state;
+	}
+	
+	public boolean HasHealthPotions()
+	{
+		return healthPotions.isEmpty() ? false : true;
+	}
+	
+	public HealthPotion GetHealthPotion()
+	{
+		return healthPotions.remove(0);
+	}
+	
+	public boolean HasPoisonPotions()
+	{
+		return poisonPotions.isEmpty() ? false : true;
+	}
+	
+	public PoisonPotion GetPoisonPotion()
+	{
+		return poisonPotions.remove(0);
+	}
+	
+	public boolean HasStrengthPotions()
+	{
+		return strengthPotions.isEmpty() ? false : true;
+	}
+	
+	public StrengthPotion GetStrengthPotion()
+	{
+		return strengthPotions.remove(0);
 	}
 }
